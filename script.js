f17a0f5a6e364397049171dfe25511a9f41e995e
Fahi-deen <fahideen786@gmail.com>
2021-12-25 17:52:44 +0530

    final_output

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnsOpenModel = document.querySelectorAll('.show-modal');
+
+const openModel = function () {
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+
+};
+const closeModal = function () {
+	modal.classList.add(`hidden`);
+	overlay.classList.add(`hidden`);
+};
+
+for (let i = 0; i < btnsOpenModel.length; i++)
+	btnsOpenModel[i].addEventListener('click', openModel);
+btnCloseModel.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener(`keydown`, function (e) {
+	if (e.key === 'Escape' && !modal.classList.contains(`hidden`)) {
+		closeModal();
+
+	}
+})
